1b6f44b6533fc5baf673bb010ba592ca5fd84d66
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:00:45 -0300

    add: comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         double raio, volume;
         
+        //entrada de dados
         raio = leia.nextDouble();
         
+        //processamento
         volume = (4/3.0) * (3.14159 * Math.pow(raio,3));
+        
+        //saida de dados 
         System.out.printf("VOLUME = %.3f\n", volume);
     }
 }
